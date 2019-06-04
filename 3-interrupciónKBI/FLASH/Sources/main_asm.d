a56b855d22438c8f499bae57899639b02412354b
Micros Unal <50812478+Microsunal@users.noreply.github.com>
2019-06-03 21:39:10 -0500

    Add files via upload

---
+++ 3-interrupciónKBI/FLASH/Sources/main_asm.d
@@ -0,0 +1,5 @@
+
+../Sources/main.o: \
+	../Sources/main.asm \
+	C:/Users/Ivan/MyNewWorkspace/3-interrupciónKBI/Project_Headers/derivative.inc \
+	C:/Users/Ivan/MyNewWorkspace/3-interrupciónKBI/Project_Headers/MC9S08QG8.inc 
